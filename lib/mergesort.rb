d419dc9f14ee7b5b3abf17eacfacf5c7f5be1bc0
Heidi Hoopes <heidih@gmail.com>
2015-12-13 23:16:01 -0700

    Complete functionality for mergesort, passes all tests.

--- lib/mergesort.rb
+++ lib/mergesort.rb
@@ -1,31 +1,34 @@
 require 'pry'
 
 module SortingSuite
+
   class MergeSort
 
-    def merge_sort(array)
-      if array.length < 1
-        return array
-
-      else
-        left, right = [], []
-        m = array.length/2
-        left = array.slice(0..m).merge_sort
-        right = array.slice(m + 1..-1).merge_sort
-
-        return merge_compare(left, right)
-      end
+    def sort(array)
+      return array if array.length <=1
+      l = array.length
+      m = array.length/2
+      left = array[0..m -1]
+      right = array[m..-1]
+      merge_compare(sort(left), sort(right))
     end
 
     def merge_compare(left, right)
       new_array = []
-      if left[first] > right[first]
-        new_array << left.shift
-      end
-      merge_compare(left, right)
-    end
 
-    def benchmark
+      while left.length > 0 && right.length > 0
+        if left[0] > right[0]
+          new_array << right.shift
+        else new_array << left.shift
+        end
+      end
+      
+      if left.empty?
+        new_array.concat right
+      else # right.empty?
+        new_array.concat left
+      end
+      new_array
     end
   end
 end
