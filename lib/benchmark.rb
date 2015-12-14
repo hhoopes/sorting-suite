c2f4c7f488c64881e8aeed5371b7ebcd838bc1c9
Heidi Hoopes <heidih@gmail.com>
2015-12-13 21:17:20 -0700

    Add benchmark functionality, including fastest and slowest.

---
+++ lib/benchmark.rb
@@ -0,0 +1,55 @@
+require_relative 'insertionsort'
+require_relative 'mergesort'
+require_relative 'bubblesort'
+
+module SortingSuite
+
+  class Benchmark
+    attr_accessor :runtimes
+
+    def initialize
+      @runtimes = Hash.new
+    end
+
+    def time(sorttype, *arr)
+      array = arr if arr
+      get_runtimes(sorttype, array)
+      output_runtimes(sorttype)
+    end
+
+    def get_runtimes(sorttype, array)
+      sorter = sorttype.new
+      starttime = Time.now
+      sorter.sort(array)
+      endtime = Time.now
+      runtime = endtime - starttime
+      @runtimes[sorttype] = runtime
+    end
+
+    def output_runtimes(sorttype)
+      puts "#{format(sorttype)} took #{@runtimes[sorttype]} seconds."
+    end
+
+    def fastest
+      return if @runtimes.length < 2
+      a = @runtimes.to_a
+      fastest = a.min_by do | pair |
+        pair.last
+      end.first
+      puts "#{format(fastest)} was the fastest"
+    end
+
+    def slowest
+      return if @runtimes.length < 2
+      a = @runtimes.to_a
+      slowest = a.max_by do | pair |
+        pair.last
+      end.first
+      puts "#{format(slowest)} was the slowest"
+    end
+
+    def format(sorttype)
+      sorttype.to_s.split("::").last
+    end
+  end
+end
--- test/benchmark_test.rb
+++ test/benchmark_test.rb
@@ -1,6 +1,34 @@
 require 'minitest/autorun'
 require 'benchmark'
+require 'shoulda/context'
 
 class SortingSuite::BenchmarkTest < Minitest::Test
+  context 'a benchmarking tool' do
 
+    setup do
+      @benchmark = SortingSuite::Benchmark.new
+      @sorttimes = @benchmark.time(SortingSuite::InsertionSort, [3,3,4,5,1])
+    end
+
+    should "output a total time after benchmarking is done" do
+      skip
+      assert_match "took",
+    end
+
+    should "reuse the first array for a second sorting function" do
+      bubble_sort = @benchmark.time(SortingSuite::BubbleSort)
+      assert_equal 2, @benchmark.runtimes.length
+    end
+
+    should "output the slowest sort function when method is run" do
+      skip
+      assert_equal "InsertionSort was the slowest", @benchmark.slowest
+    end
+
+    should "output the fastest sort function when method is run" do
+      skip
+      assert_equal "InsertionSort was the fastest", @benchmark.fastest
+    end
+
+  end
 end
